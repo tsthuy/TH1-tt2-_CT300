861530700e07e7070b56279580a24cdcf9761f3e
tsthuy <huydo282003@gmail.com>
2024-01-30 09:05:39 +0700

    change hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
+    int count = 10;
+    for (int i = 1; i <= count; i++)
+    {
+        printf("%d\n", i);
+    }
+
     printf("Hello World \n");
     return 0;
 }
